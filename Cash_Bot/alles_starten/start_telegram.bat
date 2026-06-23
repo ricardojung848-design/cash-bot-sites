2afe5e22d37476d910f02b8b5701e94bfb568814
ricardojung848-design <DEINE_ECHTE_GITHUB_EMAIL@beispiel.com>
2026-06-23 22:31:33 +0200

    feat: aktualisiere Startskript für Telegram-Bot und aktualisiere Subprojekt-Commit

--- Cash_Bot/alles_starten/start_telegram.bat
+++ Cash_Bot/alles_starten/start_telegram.bat
@@ -4,5 +4,10 @@ title Telegram Starter
 
 cd /d "C:\Users\ricoj\Desktop\Deto_Art_Agenten\Cash_Bot"
 
+REM >>> HIER DEIN TOKEN EINTRAGEN
+set "TELEGRAM_BOT_TOKEN=8905346856:AAF9x8dA-oYf-ACfheIl-j6QsMQoOlh6qbI"
+
+echo Starte Telegram-Bot...
 start "Telegram" cmd /k "python -m core.Agent_Telegram"
+
 exit
